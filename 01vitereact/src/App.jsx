776f65e7c9f73cab097c47b6e6cfda4d206dd6b6
kedarBaswade <kedarbaswade2802@gmail.com>
2025-07-02 17:54:36 +0530

    added about reactflow,variables,how react works

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,15 +1,20 @@
 import Chai from "./chai"
 
 function App() {
+  const username='Game of Thrones'
   return (
     // <h1>wasndkjn</h1> ye hum jsx me nahi kr skte we can only do single element,toh
     //ek div banake ya empty tag banake usme jitna chahe utna kro export/import  
     <div>
       <Chai/>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint illum dolore rem ut cum at qui dignissimos eum cupiditate, fugit expedita quam error, similique accusantium. Itaque reprehenderit perspiciatis natus qui placeat vero odit quo.</p>
+      <p>Lorem ipsum {username}dolor sit amet consectetur, adipisicing elit. Sint illum dolore rem ut cum at qui dignissimos eum cupiditate, fugit expedita quam error, similique accusantium. Itaque reprehenderit perspiciatis natus qui placeat vero odit quo.</p>
       </div>
       //or <> iske andar bhi kr skte he div likhane ki jarurat nahi he</>
   )
 }
 
 export default App
+
+
+//{variables} curly braces me likha hua sab kuch as a variable count hota he
+//ise kehte he expression/evaluated expression {} iske andar hum direct final outcome hi likhate he
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,49 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function myAPP(){
+  return(
+    <div>
+      <h1 >Custom App!</h1>              
+    </div>
+  )
+}
+
+// const reactElement={
+//     type :'a', 
+//     props : { 
+//             href: 'https://google.com',
+//             target : '_blank'
+//     },
+//     children : 'click me to visit google'
+// }
+
+//{variables} curly braces me likha hua sab kuch as a variable count hota he
+//ise kehte he expression/evaluated expression {} iske andar hum direct final outcome hi likhate he
+
+const anotherName="Chai aur react"
+const anotherElement=(
+  <a href="https://google.com" target='_blank'>click me to visit google</a>
+)
+
+//generally hum jab ye function return krte he tab uska aise hi object behind the scenes react banata he aur use inject kr deta he
+
+//ye react ka structure he hamara wala object nahi chala kyunki usme hamne hamare hisab se keywords/syntax use kiya pr, ye directly use kr skte he kyunki ye react ka he  ,variable last me rehta he
+const reactElement=React.createElement(
+  'a',
+  {href:'https://goggle.com',target:'_blank'},
+  'click me to visit google',
+  anotherName
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <>
+  //   <myAPP/> //  ye he toh function hi toh isse hum myApp()aise bhi likh skte he,par likhana nahi he henerally <> aise hi likha jata he</>
+  //   <App />
+  // </>  
+
+ //   anotherElement 
+
+ reactElement
 )
