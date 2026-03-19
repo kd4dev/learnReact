067b4922d050912b412a3521fce0034ca92b603b
Kedar Baswade <kedarbaswade2802@gmail.com>
2026-03-19 17:13:09 +0530

    Delete 01vitereact directory

--- 01vitereact/src/chai.jsx
+++
@@ -1,9 +0,0 @@
- function Chai(){
-    return(
-        <h3> chai is ready </h3>
-    )
- }
-export default Chai
-
-//element ka naam hamesha capital se start karo chahe react-create-app,ho ya 
-//vite ho 
